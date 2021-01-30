0422fe4b6415b92af8bdaf71cb40a847e2f11b8f
Edwin Nava <edwinavahh@outlook.com>
2021-01-30 02:40:28 -0700

    script

---
+++ script.sh
@@ -0,0 +1,12 @@
+cd /home/debian
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+apt-get install docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose version
+git clone git@github.com:iProxys/caseonalize.git
+cd caseonalize
+docker image build . -t caseonalize
+docker-compose up
